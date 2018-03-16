5728be81923495cd5a6f7b29670393c3671f3be6
Benjamin Geyer <brgeyer49@gmail.com>
2018-03-16 15:47:59 -0400

    Initial Commit

--- src/demos/Login.java
+++ src/demos/Login.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Login {
 
     public static void main(String[] args) {
+        
         // 1. define the web driver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\brgey\\SDET\\Software\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
